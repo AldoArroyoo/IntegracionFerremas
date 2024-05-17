14ad2f2d80a94dc05c563718d1c8d11e227e2d27
GiovanniBarra <gio.barra@duocuc.cl>
2024-05-17 15:39:00 -0400

    arreglos de models

--- src/controller/productoController.js
+++ src/controller/productoController.js
@@ -5,6 +5,7 @@ const Tipo_producto = require("../models/Tipo_producto.js")
 const Marca = require("../models/Marca.js")
 const Modelo = require("../models/Modelo.js")
 const Estado = require("../models/Estado.js")
+const { query } = require("express")
 
 class ErrorTraerPorCategoria extends Error {
     constructor(mensaje) {
@@ -41,7 +42,7 @@ Producto.traerPorCategoria = async (request, response) => {
 
     try {
         connection = await abrirConexion()
-
+console.log("abri la coneccion")
         var Query = `SELECT p.cod_producto, 
                             p.nom_producto, 
                             p.precio, 
@@ -56,30 +57,30 @@ Producto.traerPorCategoria = async (request, response) => {
                             ma.nom_marca,
                             e.nom_estado,
                             c.nom_categoria
-                    FROM CATEGORIA AS c 
-                    JOIN TIPO_PRODUCTO AS tp ON (c.cod_categoria = tp.cod_categoria)
-                    JOIN PRODUCTO AS p ON (tp.cod_tipo = p.cod_tipo) 
+                    FROM MARCA AS ma 
+                    JOIN MODELO AS m ON (ma.cod_marca = m.cod_marca)
+                    JOIN PRODUCTO AS p ON (m.cod_modelo = p.cod_modelo)
                     JOIN ESTADO AS e ON (p.cod_estado = e.cod_estado)
-                    JOIN MODELO AS m ON (p.cod_modelo = m.cod_modelo)
-                    JOIN MARCA AS ma ON (m.cod_marca = ma.cod_marca)
+                    JOIN TIPO_PRODUCTO AS tp ON (p.cod_tipo = tp.cod_tipo)
+                    JOIN CATEGORIA AS c ON (p.cod_categoria = c.cod_categoria)
                     WHERE c.cod_categoria = ?`
-
+console.log("pase por aqui")
         const values = [cod_categoria]
         
         const [filas, otro] = await connection.query(Query, values)
-        console.log(values)
+        console.log("pase el query")
         if (filas.length === 0) {
             throw new ErrorTraerPorCategoria('No hay productos en esta categoría')
         }
 
         filas.map(f => {
             var categoria = new Categoria (cod_categoria, f.nom_categoria)
-            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo, categoria)
+            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo)
             var marca = new Marca (f.cod_marca, f.nom_marca)
             var modelo = new Modelo (f.cod_modelo, f.nom_modelo, marca)
             var estado = new Estado (f.cod_estado, f.nom_estado)
             var producto = new Producto (f.cod_producto, f.nom_producto, f.codigo, f.precio,
-                                        f.descuento, tipo, modelo, estado)
+                                        f.descuento, tipo, modelo, estado, categoria)
 console.log("ya pase")
                 productos.push(producto.to_json())
                 console.log("ya pase")
@@ -87,7 +88,7 @@ console.log("ya pase")
 
         return response.status(202).json(productos)
     } catch (error) {
-
+console.log(error)
         if (error instanceof ErrorDBA) {
             return response.status(500).json(error.message)
 
@@ -129,30 +130,32 @@ Producto.traerPorEstado = async (request, response) => {
                             e.nom_estado,
                             c.nom_categoria,
                             c.cod_categoria
-                    FROM CATEGORIA AS c 
-                    JOIN TIPO_PRODUCTO AS tp ON (c.cod_categoria = tp.cod_categoria)
-                    JOIN PRODUCTO AS p ON (tp.cod_tipo = p.cod_tipo) 
+                    FROM MARCA AS ma 
+                    JOIN MODELO AS m ON (ma.cod_marca = m.cod_marca)
+                    JOIN PRODUCTO AS p ON (m.cod_modelo = p.cod_modelo)
                     JOIN ESTADO AS e ON (p.cod_estado = e.cod_estado)
-                    JOIN MODELO AS m ON (p.cod_modelo = m.cod_modelo)
-                    JOIN MARCA AS ma ON (m.cod_marca = ma.cod_marca)
+                    JOIN TIPO_PRODUCTO AS tp ON (p.cod_tipo = tp.cod_tipo)
+                    JOIN CATEGORIA AS c ON (p.cod_categoria = c.cod_categoria)
                     WHERE p.cod_estado = ?`
 
                     const values = [cod_estado]
+
+                    console.log(values)
         
         const [filas, otro] = await connection.query(Query, values)
-        console.log(values)
+        console.log(filas)
         if (filas.length === 0) {
             throw new ErrorTraerPorEstado("No hay productos con este estado")
         }
 
         filas.map(f => {
             var categoria = new Categoria (f.cod_categoria, f.nom_categoria)
-            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo, categoria)
+            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo)
             var marca = new Marca (f.cod_marca, f.nom_marca)
             var modelo = new Modelo (f.cod_modelo, f.nom_modelo, marca)
             var estado = new Estado (cod_estado, f.nom_estado)
             var producto = new Producto (f.cod_producto, f.nom_producto, f.codigo, f.precio,
-                                        f.descuento, tipo, modelo, estado)
+                                        f.descuento, tipo, modelo, estado, categoria)
 
                 productos.push(producto.to_json())
 
@@ -162,7 +165,7 @@ Producto.traerPorEstado = async (request, response) => {
 
 
     } catch (error) {
-
+console.log(error)
         if (error instanceof ErrorDBA) {
             return response.status(500).json(error.message)
 
@@ -205,12 +208,12 @@ Producto.traerPorMarca = async (request, response) => {
                             e.nom_estado,
                             c.nom_categoria,
                             c.cod_categoria
-                    FROM CATEGORIA AS c 
-                    JOIN TIPO_PRODUCTO AS tp ON (c.cod_categoria = tp.cod_categoria)
-                    JOIN PRODUCTO AS p ON (tp.cod_tipo = p.cod_tipo) 
+                    FROM MARCA AS ma 
+                    JOIN MODELO AS m ON (ma.cod_marca = m.cod_marca)
+                    JOIN PRODUCTO AS p ON (m.cod_modelo = p.cod_modelo)
                     JOIN ESTADO AS e ON (p.cod_estado = e.cod_estado)
-                    JOIN MODELO AS m ON (p.cod_modelo = m.cod_modelo)
-                    JOIN MARCA AS ma ON (m.cod_marca = ma.cod_marca)
+                    JOIN TIPO_PRODUCTO AS tp ON (p.cod_tipo = tp.cod_tipo)
+                    JOIN CATEGORIA AS c ON (p.cod_categoria = c.cod_categoria)
                     WHERE ma.cod_marca = ?`
 
                     const values = [cod_marca]
@@ -223,12 +226,12 @@ Producto.traerPorMarca = async (request, response) => {
 
         filas.map(f => {
             var categoria = new Categoria (f.cod_categoria, f.nom_categoria)
-            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo, categoria)
+            var tipo = new Tipo_producto (f.cod_tipo, f.nom_tipo)
             var marca = new Marca (cod_marca, f.nom_marca)
             var modelo = new Modelo (f.cod_modelo, f.nom_modelo, marca)
             var estado = new Estado (f.cod_estado, f.cod_estado)
             var producto = new Producto (f.cod_producto, f.nom_producto, f.codigo, f.precio,
-                                        f.descuento, tipo, modelo, categoria, estado)
+                                        f.descuento, tipo, modelo, estado, categoria)
 
                 productos.push(producto.to_json())
 
@@ -281,13 +284,13 @@ Producto.traerEspecifico = async (request, response) => {
                             e.nom_estado,
                             c.nom_categoria,
                             c.cod_categoria
-                    FROM CATEGORIA AS c 
-                    JOIN TIPO_PRODUCTO AS tp ON (c.cod_categoria = tp.cod_categoria)
-                    JOIN PRODUCTO AS p ON (tp.cod_tipo = p.cod_tipo) 
+                    FROM MARCA AS ma 
+                    JOIN MODELO AS m ON (ma.cod_marca = m.cod_marca)
+                    JOIN PRODUCTO AS p ON (m.cod_modelo = p.cod_modelo)
                     JOIN ESTADO AS e ON (p.cod_estado = e.cod_estado)
-                    JOIN MODELO AS m ON (p.cod_modelo = m.cod_modelo)
-                    JOIN MARCA AS ma ON (m.cod_marca = ma.cod_marca)
-                    WHERE p.cod_producto = ?`
+                    JOIN TIPO_PRODUCTO AS tp ON (p.cod_tipo = tp.cod_tipo)
+                    JOIN CATEGORIA AS c ON (p.cod_categoria = c.cod_categoria)
+                    WHERE c.cod_categoria = ?`
 
                     const values = [cod_producto]
         
@@ -334,7 +337,40 @@ console.log(error)
 
 
 
+Producto.modificar = async (request, response) => {
+    const { cod_estado, precio, descuento, cod_producto } = request.body
+    
+    var connection = null
 
+    try {
+        connection = await abrirConexion()
+
+        var Query = `UPDATE PRODUCTO
+                    SET cod_estado = ?, precio = ?, descuento = ?
+                    WHERE cod_producto = ?`
+        
+        values = [cod_estado, precio, descuento, cod_producto]
+
+        const [filas, otro] = await connection.query(Query, values)
+
+    } catch (error) {
+        console.log(error)
+                if (error instanceof ErrorDBA) {
+                    return response.status(500).json(error.message)
+        
+                }else if (error instanceof ErrorTraerEspecifico) {
+                    return response.status(500).json(error.message)
+        
+                } else {
+                    return response.status(500).json(error)
+                }
+            }
+            finally {
+                if (connection) {
+                    connection.release()
+                }
+            }
+        };
 
 
 module.exports = Producto
